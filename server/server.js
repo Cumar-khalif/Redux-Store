6c1c3091b448625ec1df7a6a99a6842dd35c9239
Omar Abdirahman S <73026620+Cumar-khalif@users.noreply.github.com>
2021-12-08 21:32:33 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -31,7 +31,7 @@ app.get('*', (req, res) => {
 });
 
 db.once('open', () => {
-  app.listen( process.env.PORT || 3001, function() {
+  app.listen( PORT || 3001, function() {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
